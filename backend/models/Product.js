78d639978da4eb4e6dfe07bc6155174eb6fc0abf
hidesusanto <vosonhung03@gmail.com>
2023-12-28 14:14:39 +0700

    fix models and add category model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,19 +1,12 @@
-const mongoose = require('mongoose');
-
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  decription: {
-    type: String,
-    required: false
-  }
+  name: String,
+  description: String,
+  price: Number,
+  images: [{ type: String }], // Sử dụng mảng để lưu trữ nhiều hình ảnh
+  quantity: Number,
+  attributes: [{ size: String, color: String }],
+  category: String,
 });
 
-module.exports = mongoose.model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
