f0ed196bbaaf70bdf887df13793e6c2359e3deef
hidesusanto <vosonhung03@gmail.com>
2023-12-29 13:54:39 +0700

    render product to home page

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,81 +1,22 @@
+import axios from 'axios';
 import ProductBadge from "./ProductBadge";
-
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    decription: "Used for summer vacation is the best choice",
-    price: '$35',
-  },
-  {
-      id: 2,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$352',
-    },
-    {
-      id: 3,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-    {
-      id: 4,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-    {
-      id: 5,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-    {
-      id: 7,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-    {
-      id: 8,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-    {
-      id: 9,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      decription: "Used for summer vacation is the best choice",
-      price: '$35',
-    },
-  // More products...
-]
+import {useState, useEffect} from "react";
 const Content = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      let res = await axios.get('http://localhost:8000/api/products')
+      setProducts(res.data);
+      console.log(res.data); // Log dữ liệu trực tiếp từ response
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []); // Sử dụng mảng rỗng để đảm bảo hàm chỉ chạy một lần sau khi component được mount
     return ( 
       <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -83,14 +24,11 @@ const Content = () => {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <ProductBadge key={product.id}
-            id = {product.id} 
-            name = {product.name} 
-            href={product.href} 
-            imageSrc={product.imageSrc}
-            imageAlt={product.imageAlt}
-            price={product.price}
-            decription={product.decription}>
+            <ProductBadge key={product._id}
+            id = {product._id} 
+            name = {product.name}  
+            image={product.images[0]}
+            price={product.price}>
             </ProductBadge>
           ))}
         </div>
--- frontend/src/components/ProductBadge.jsx
+++ frontend/src/components/ProductBadge.jsx
@@ -1,27 +1,26 @@
 
-  const ProductBadge = ({id, name, href, imageSrc, imageAlt, decription, price}) => {
+  const ProductBadge = ({id, name, image, price}) => {
       return ( 
               <div key={id} className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <img
-                    src={imageSrc}
-                    alt={imageAlt}
+                    src={image}
+                    alt="nvm"
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <a href={href}>
+                      <a>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {name}
                       </a>
                     </h3>
                     
                   </div> 
-                  <p className="text-sm font-medium text-gray-900">{price}</p>
+                  <p className="text-sm font-medium text-gray-900">${price}</p>
                 </div>
-                <sub>{decription}</sub>
               </div>
        );
   }
